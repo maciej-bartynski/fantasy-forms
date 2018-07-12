a409e4405cc594b46288bc94f81fa702c8f404f8
unknown <maciek.bartynski@gmail.com>
2018-07-12 19:14:37 +0200

    stylize

--- source/dynamics/ataki-set-txt.js
+++ source/dynamics/ataki-set-txt.js
@@ -1,127 +1,139 @@
-"use strict";
-document.addEventListener("DOMContentLoaded", initialize);
-function initialize() {
+'use strict'
+document.addEventListener('DOMContentLoaded', initialize)
+function initialize () {
   let opts = document.querySelectorAll(
-    ".corpus_section_form_fields_fieldset-b_container_select-container_select-list option"
-  );
-  let amount = opts.length;
+    '.corpus_section_form_fields_fieldset-b_container_select-container_select-list option'
+  )
+  let amount = opts.length
   for (let i = 0; i < amount; i++) {
-    let item = opts[i];
-    item.addEventListener("click", function() {
-      setIMG(i);
-      setStrikeNameToDes(i);
-    });
+    let item = opts[i]
+    item.addEventListener('click', function () {
+      setIMG(i)
+      setStrikeNameToDes(i)
+    })
   }
 }
-function setPartOfAttackDescription(i) {
-  let desPart = document.querySelector("p span.--des_epitet");
+function setPartOfAttackDescription (i) {
+  let desPart = document.querySelector('p span.--des_epitet')
   let array = [
-    "brutalne",
-    "nieprzewidywalne",
-    "wyćwiczone",
-    "niezawodne",
-    "precyzyjne",
-    "zmasowane",
-    "podstępne",
-    "wyrachowane",
-    "zdradzieckie",
-    "szaleńcze",
-    "opracowane w laboratorium alchemicznym",
-    "niepowstrzymane",
-    "władcze",
-    "mroczne",
-    "tajemne",
-    "wściekłe",
-    "wspierane mocą otchłani",
-    "przesycone złą mocą"
-  ];
-  desPart.innerText = array[i];
+    'brutalne',
+    'nieprzewidywalne',
+    'wyćwiczone',
+    'niezawodne',
+    'precyzyjne',
+    'zmasowane',
+    'podstępne',
+    'wyrachowane',
+    'zdradzieckie',
+    'szaleńcze',
+    'opracowane w laboratorium alchemicznym',
+    'niepowstrzymane',
+    'władcze',
+    'mroczne',
+    'tajemne',
+    'wściekłe',
+    'wspierane mocą otchłani',
+    'przesycone złą mocą'
+  ]
+  desPart.innerText = array[i]
 }
-function setIMG(i) {
+function setIMG (i) {
   let belt = document.querySelectorAll(
-    ".corpus_section_form_fields_fieldset-b_container_select-container_img-part-container_opt"
-  )[i];
-  let imag = belt.querySelectorAll("img")[0];
-  let attryb = imag.getAttribute("src");
-  let icon = document.querySelector(".--plate_img_icon");
-  icon.setAttribute("src", attryb);
-  let allIMGs = belt.querySelectorAll("img").length;
-  let standart = document.querySelector(".--standart_img_bckg");
-  while (standart.querySelector("IMG") !== null) {
-    let imageToDel = standart.querySelector("IMG");
-    standart.removeChild(imageToDel);
+    '.corpus_section_form_fields_fieldset-b_container_select-container_img-part-container_opt'
+  )[i]
+  let imag = belt.querySelectorAll('img')[0]
+  let attryb = imag.getAttribute('src')
+  let icon = document.querySelector('.--plate_img_icon')
+  icon.setAttribute('src', attryb)
+  let allIMGs = belt.querySelectorAll('img').length
+  let standart = document.querySelector('.--standart_img_bckg')
+  while (standart.querySelector('IMG') !== null) {
+    let imageToDel = standart.querySelector('IMG')
+    standart.removeChild(imageToDel)
   }
   for (let j = 0; j < allIMGs; j++) {
     if (j > 0) {
-      let theIMG = belt.querySelectorAll("img")[j];
-      let sourceIMG = theIMG.getAttribute("src");
-      let newIMG = document.createElement("img");
-      newIMG.setAttribute("src", sourceIMG);
-      standart.appendChild(newIMG);
+      let theIMG = belt.querySelectorAll('img')[j]
+      let sourceIMG = theIMG.getAttribute('src')
+      let newIMG = document.createElement('img')
+      newIMG.setAttribute('src', sourceIMG)
+      standart.appendChild(newIMG)
     }
   }
 }
-function setNameToDes() {
-  let inp = document.querySelector('input[name="imie"]');
-  let nam = inp.value;
-  let inpB = document.querySelector('input[name="przydomek"]');
-  let surnam = inpB.value;
-  let item = document.querySelector(".--des_imie");
-  item.innerText = nam + " " + surnam;
+function setNameToDes () {
+  let inp = document.querySelector('input[name="imie"]')
+  let nam = inp.value
+  let inpB = document.querySelector('input[name="przydomek"]')
+  let surnam = inpB.value
+  let item = document.querySelector('.--des_imie')
+  item.innerText = nam + ' ' + surnam
 }
-function setStrikeNameToDes(i) {
-  let inp = document.querySelector('input[name="nazwauderzenia"]');
-  inp.addEventListener("change", function() {
-    let strName = inp.value;
-    let item = document.querySelector(".--des_nazwa-ciosu");
-    item.innerText = strName;
-    setPartOfAttackDescription(i);
-    setNameToDes();
-    setForceDes(i);
-    showAllDes();
-  });
+function setStrikeNameToDes (i) {
+  let inp = document.querySelector('input[name="nazwauderzenia"]')
+  inp.addEventListener('change', function () {
+    let strName = inp.value
+    let item = document.querySelector('.--des_nazwa-ciosu')
+    item.innerText = strName
+    setPartOfAttackDescription(i)
+    setNameToDes()
+    setForceDes(i)
+    showAllDes()
+  })
+  inp.addEventListener('click', function () {
+    let itm = inp.value
+    if (itm.trim() !== '') {
+      let strName = inp.value
+      let item = document.querySelector('.--des_nazwa-ciosu')
+      item.innerText = strName
+      setPartOfAttackDescription(i)
+      setNameToDes()
+      setForceDes(i)
+      showAllDes()
+    }
+  })
 }
-function setForceDes(i) {
+function setForceDes (i) {
   let belt = document.querySelectorAll(
-    ".corpus_section_form_fields_fieldset-b_container_select-container_img-part-container_opt"
-  )[i];
-  let IMGs = belt.querySelectorAll("img");
-  let iter = IMGs.length;
-  let strng = [];
+    '.corpus_section_form_fields_fieldset-b_container_select-container_img-part-container_opt'
+  )[i]
+  let IMGs = belt.querySelectorAll('img')
+  let iter = IMGs.length
+  let strng = []
   for (let j = 0; j < iter; j++) {
-    let IMG = IMGs[j];
-    let attryb = IMG.getAttribute("src");
+    let IMG = IMGs[j]
+    let attryb = IMG.getAttribute('src')
     if (j !== 0) {
-      if (attryb === "icons/bron-barba.svg") {
-        strng.push("dodatkową mocą uderzeniową");
-      } else if (attryb === "icons/bron-czar.svg") {
-        strng.push("dodatkową mocą czarnoksięską");
-      } else if (attryb === "icons/bron-strz.svg") {
-        strng.push("dodatkowym kunsztem strzeleckim");
-      } else if (attryb === "icons/bron-szal.svg") {
-        strng.push("dodatkowym szaleństwem");
-      } else if (attryb === "icons/bron-zdra.svg") {
-        strng.push("niespodzianym zdradliwym ciosem");
-      } else if (attryb === "icons/zyw-ogien.svg") {
-        strng.push("żywiołem ognia");
-      } else if (attryb === "icons/zyw-rozklad.svg") {
-        strng.push("żywiołem rozkładu");
-      } else if (attryb === "icons/zyw-wod.svg") {
-        strng.push("żywiołem wody");
-      } else if (attryb === "icons/zyw-zmiana.svg") {
-        strng.push("żywiołem zmiany");
-      } else if (attryb === "icons/zyw-zywia.svg") {
-        strng.push("żywiołem żywii");
+      if (attryb === 'icons/bron-barba.svg') {
+        strng.push('dodatkową mocą uderzeniową')
+      } else if (attryb === 'icons/bron-czar.svg') {
+        strng.push('dodatkową mocą czarnoksięską')
+      } else if (attryb === 'icons/bron-strz.svg') {
+        strng.push('dodatkowym kunsztem strzeleckim')
+      } else if (attryb === 'icons/bron-szal.svg') {
+        strng.push('dodatkowym szaleństwem')
+      } else if (attryb === 'icons/bron-zdra.svg') {
+        strng.push('niespodzianym zdradliwym ciosem')
+      } else if (attryb === 'icons/zyw-ogien.svg') {
+        strng.push('żywiołem ognia')
+      } else if (attryb === 'icons/zyw-rozklad.svg') {
+        strng.push('żywiołem rozkładu')
+      } else if (attryb === 'icons/zyw-wod.svg') {
+        strng.push('żywiołem wody')
+      } else if (attryb === 'icons/zyw-zmiana.svg') {
+        strng.push('żywiołem zmiany')
+      } else if (attryb === 'icons/zyw-zywia.svg') {
+        strng.push('żywiołem żywii')
       } else {
-        strng.push("własną mądrością żywiołów i talentów");
+        strng.push('własną mądrością żywiołów i talentów')
       }
     }
   }
-  let stringToSet = strng.join(", ");
-  let spnDes = document.querySelector(".--des_zywiol");
-  spnDes.innerText = stringToSet + ".";
+  let stringToSet = strng.join(', ')
+  let spnDes = document.querySelector('.--des_zywiol')
+  spnDes.innerText = stringToSet + '.'
 }
-function showAllDes() {
-  let allDes = document.querySelector(".--des");
-  allDes.classList.remove("itsHidden");
+function showAllDes () {
+  let allDes = document.querySelector('.--des')
+  allDes.classList.remove('itsHidden')
 }
--- source/dynamics/podstawy.js
+++ source/dynamics/podstawy.js
@@ -1,33 +1,67 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function (){
-    userFlowViaFirstFieldset();
+'use strict'
+document.addEventListener('DOMContentLoaded', function () {
+  userFlowViaFirstFieldset()
 })
-function userFlowViaFirstFieldset(){
+function userFlowViaFirstFieldset () {
+  let userName = document.querySelector('input[name="imie"]');
+  let userNick = document.querySelector('input[name="przydomek"]');
+  let userMotto = document.querySelector('textarea[name="zawolanie"]');
+  userNameAccept(userName, userNick)
+  userNickAccept(userNick, userMotto)
+  userMottoAccept(userMotto, userName)
+}
+function userNameAccept (item, nextItem) {
+  item.addEventListener('keyup', function (event) {
+    let allFieldsAreSet = checkIfFieldsAreSet();
+    if (allFieldsAreSet === true) {
+      enableNextPartOfFormula();
+    } else if (event.keyCode === 13) {
+      nextItem.focus()
+    }
+  })
+}
+function userNickAccept (item, nextItem) {
+  item.addEventListener('keyup', function (event) {
+    let allFieldsAreSet = checkIfFieldsAreSet();
+    if (allFieldsAreSet === true) {
+      enableNextPartOfFormula();
+    } else if (event.keyCode === 13) {
+      nextItem.focus()
+    }
+  })
+}
+function userMottoAccept (item, nextItem) {
+  item.addEventListener('keyup', function (event) {
+    let allFieldsAreSet = checkIfFieldsAreSet();
+    if (allFieldsAreSet === true) {
+      enableNextPartOfFormula();
+    } else if (event.keyCode === 13) {
+      nextItem.focus()
+    }
+  })
+}
+function checkIfFieldsAreSet() {
     let userName = document.querySelector('input[name="imie"]');
     let userNick = document.querySelector('input[name="przydomek"]');
     let userMotto = document.querySelector('textarea[name="zawolanie"]');
-    userNameAccept(userName, userNick);
-    userNickAccept(userNick, userMotto);
-    userMottoAccept(userMotto, userName);
-}
-function userNameAccept(item, nextItem){
-    item.addEventListener('keypress', function (event) {
-        if(event.keyCode===13){
-            nextItem.focus();
+    let valueA = userName.value;
+    let valueB = userNick.value;
+    let valueC = userMotto.value;
+    if (valueA.trim()!==""&&valueA.trim().toLowerCase()!=="heros"){
+        if(valueB.trim()!==""&&valueB.trim().toLowerCase()!=="wojenny pies"){
+          if(valueC.trim()!==""&&valueC.trim().toLowerCase()!=="zawsze wierny"){
+            return true;
+          }  
         }
-    });
+    }
 }
-function userNickAccept(item, nextItem){
-    item.addEventListener('keypress', function (event) {
-        if(event.keyCode===13){
-            nextItem.focus();
-        }
-    });
-}
-function userMottoAccept(item, nextItem){
-    item.addEventListener('keypress', function (event) {
-        if(event.keyCode===13){
-            nextItem.focus();
-        }
-    });
+function enableNextPartOfFormula(){
+    let thisOrnament = document.querySelector('img.corpus_section_form_ornament-marker');
+    let allOrnaments = document.querySelectorAll('img.corpus_section_form_ornament-marker');
+    thisOrnament.setAttribute('src', './icons/pole.svg');
+    thisOrnament.classList.add('itIsPassedThrought');
+    let nextOrnament = allOrnaments[1];
+    nextOrnament.classList.remove('itIsHidden');
+    let partOfForm = document.querySelector('.corpus_section_form_fields');
+    partOfForm.classList.remove('itIsHidden');
 }
